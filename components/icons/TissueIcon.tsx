c201ced7b2f3b2a279c7eb34372f3de3e6f82cec
kkkraven <86185793+kkkraven@users.noreply.github.com>
2025-06-06 17:58:25 +0400

    Fix README layout and correct Tissue icon

--- components/icons/TissueIcon.tsx
+++ components/icons/TissueIcon.tsx
@@ -13,7 +13,7 @@ export const TissueIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   >
     <path d="M20 6L8.27 6a4 4 0 00-3.95 3.08L3 14"></path>
     <path d="M4 18h14a2 2 0 002-2V8"></path>
-    <path d="M17 18a2 2 0 100-4h- период"></path> 
+  <path d="M17 18a2 2 0 0 0 0-4h-6"></path>
     <path d="M12 18v-4"></path>
     <path d="M6 18v-4"></path>
     <path d="M20 12.88V14a2 2 0 01-2 2H6.83"></path>
